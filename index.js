8db1197a5e8af1341ed18895e0851fef61696bd8
Badea Alamidi <badeaalamidi@gmail.com>
2022-01-06 00:43:04 -0600

    re-add original variable to 1st parameter of server.listen

--- index.js
+++ index.js
@@ -50,6 +50,6 @@ const server = http.createServer((req,res)=>{
 		}
 	);
 });
- const PORT = process.env.PORT || 5000;
-//const PORT = {host:'0.0.0.0', port: 5000};
+// const PORT = process.env.PORT || 5000;
+const PORT = {host:'0.0.0.0', port: 5000};
 server.listen(PORT, ()=> console.log(`Server running on port ${PORT.port} `));
